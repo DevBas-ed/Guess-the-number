c9dabbec5370c8c5e456608b1ff488e944f7e55b
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2022-10-02 19:58:35 +0530

    Added function to change background color.

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ let highscore=0;
 function displayMessage(message){
     document.querySelector('.message').textContent=message;
 }
+function changeBG(x){
+    document.querySelector(`body`).style.backgroundColor=x;
+}
