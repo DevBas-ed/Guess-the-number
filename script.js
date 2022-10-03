0b9fe7f3addb43b8526d14072d4f60062b556458
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2022-10-03 19:54:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,45 @@ function displayMessage(message){
 function changeBG(x){
     document.querySelector(`body`).style.backgroundColor=x;
 }
+document.querySelector(`.check`).addEventListener(`click`, function(){
+    let guess=Number(document.querySelector(`.guess`).value);
+    console.log(guess);
+    //No input
+    if(!guess){
+        document.querySelector(`.message`).textContent=`No Number!`;
+    }
+    //Player wins
+    else if(guess===number){
+
+
+        document.querySelector(`.number`).textContent=number;
+
+
+        displayMessage(`Correct Number!`);
+        changeBG(`green`);
+        if(score>highscore){
+            highscore=score;
+        }
+        document.querySelector(`.highscore`).textContent=highscore;
+    }
+    //Guess is too low
+    else if(score>1){
+    if(guess<number){
+        displayMessage(`Number is lower`);
+        score--;
+        document.querySelector(`.score`).textContent=score;
+        changeBG(`skyblue`);
+    }
+    //Guess is too high
+    else if(guess>number){
+        displayMessage(`Number is higher`);
+        score--;
+        document.querySelector(`.score`).textContent=score;
+        changeBG(`yellow`);
+    }}
+    //Player lost 
+    else{
+        displayMessage(`You lost`);
+        document.querySelector(`.score`).textContent=0;
+        changeBG(`red`);   }
+});
